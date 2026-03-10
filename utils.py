0fbbf2394ea241b547db63a2bfcc61b2cb339f13
Eli Shirley <eshirley2000@gmail.com>
2026-03-10 05:00:08 -0500

    Added some log functionality with plans to add functionality to search for a specific log. After logging, we'll add simple data management that be done using the command line.

--- utils.py
+++ utils.py
@@ -34,4 +34,28 @@ def grab_subphrase(phrase: str, key: str) -> str:
 
 # Creation and Editing of Log Files
 
-##
+## global log number
+log_number = 0
+
+## create a new log
+def get_logtime() -> str:
+    timenow = localtime()
+    outbox = ""
+    for i in range(0, len(timenow)-2):
+        outbox = outbox + "." + str(timenow[i])
+    return outbox
+
+def create_new_log(log_name: str, initial_line: str) -> tuple:
+    timenow = get_logtime()
+    path = getcwd() + "\\logs"
+    file = log_name + timenow + ".txt"
+    io.create_new_file(path, file, f"# Log Number: {log_number} @ {timenow}" + " Descriptor: " + initial_line)
+    
+    return (path, file)
+
+def log_to(log_location: str, log_name: str, message_author: str, log_message: str):
+    timenow = localtime()
+    outbox_message = f"[By {message_author} on {timenow.tm_mon}.{timenow.tm_mday}.{timenow.tm_year} @ {timenow.tm_hour}:{timenow.tm_min}:{timenow.tm_sec}]: "
+    outbox_message = outbox_message + log_message
+    outbox = io.append_lines(log_location, log_name, [outbox_message])
+    return outbox
