8e20df08d933a725c0f60b37da8eee885bfe2979
Eli Shirley <eshirley2000@gmail.com>
2026-03-12 23:18:21 -0500

    Many updates ... changed subphrase finder to include max msg length, added requirements file which can store variables necessary even after closing the application

--- utils.py
+++ utils.py
@@ -27,10 +27,49 @@ class command_builder:
 
 # Search after keyphrase through a string
 ## returns everything after the given command key
-def grab_subphrase(phrase: str, key: str) -> str:
+def grab_subphrase(phrase: str, key: str, max_length = None) -> str:
     if not key in phrase:
         return None ## specified key was not present, handles 1 expection
-    return phrase[ phrase.index(key) + len(key): ] ## return everything after the key
+    if max_length:
+        ## do something
+    else:
+        max_length = len(phrase)
+    return phrase[ phrase.index(key) + len(key):max_length ] ## return everything after the key, up to max length
+
+# Reading variables from files into memory
+
+## Initialize the required_variables file
+## which contains saved information so the program can run next time
+def initialize_reqfile() -> str:
+    directory = getcwd() + "\\req"
+    filename = "req_vars.data"
+    is_file_missing = io.create_new_file(directory, filename, "# description: Required Variables: All needed during runtime")
+    if not is_file_missing: 
+        ## check if it is empty
+        with open(directory + "\\" + filename, 'r') as file:
+            values = file.readlines()
+        if values != None:
+            return "fail: File nonempty"
+    initial_data = [
+        "{ last_opened: 2 }",
+        "last_opened: None"
+    ]
+    io.append_lines(directory, filename, initial_data)
+    return "succ: Initialized fully."
+
+## look in the req_file for a specfic variable 
+def pull_var_from_file(file_location, file_name, variable_name, expected_type):
+    lines = io.read_all(file_location, file_name)
+    if variable_name not in lines[1]:
+        ## search for the variable name in any line
+        index = 0
+        for i in range(0, len(lines)):
+            if variable_name in lines[i]:
+                index = i
+                break
+    else:
+        ## grab the index from the string associated with var
+        
 
 # Creation and Editing of Log Files
 
